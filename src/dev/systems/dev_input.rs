e20541af954351169f9b0a4f5ef5415562535ad1
Wesley Feldsine <laocoon7@gmail.com>
2025-03-05 13:17:55 -0800

    cleanup dev_input

--- src/dev/systems/dev_input.rs
+++ src/dev/systems/dev_input.rs
@@ -11,35 +11,35 @@ const SPEED: f32 = 1.0; // Acceleration speed
 pub fn dev_input(
     time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut Velocity, &mut DragCurve), With<Player>>,
+    q_velocity: Single<(&mut Velocity, &mut DragCurve), With<Player>>,
 ) {
-    for (mut velocity, mut drag) in query.iter_mut() {
-        let mut direction = Vec2::ZERO;
+    let mut direction = Vec2::ZERO;
 
-        if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
-            direction.y += 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
-            direction.y -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft) {
-            direction.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
-            direction.x += 1.0;
-        }
+    if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
+        direction.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
+        direction.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction.x += 1.0;
+    }
 
-        if direction != Vec2::ZERO {
-            // Normalize the direction vector to ensure consistent speed in all directions
-            direction = direction.normalize();
+    let (mut velocity, mut drag) = q_velocity.into_inner();
 
-            // TODO: Change this to use an acceleration curve
-            velocity.value += direction * DISTANCE * SPEED;
-            // We are adding more velocity this frame, so reset the drag timer
-            drag.elapsed_time = 0.0
-        } else {
-            // We are not adding more velocity this frame, so increment the drag timer
-            drag.elapsed_time += time.delta_secs();
-        }
+    if direction != Vec2::ZERO {
+        // Normalize the direction vector to ensure consistent speed in all directions
+        direction = direction.normalize();
+
+        // TODO: Change this to use an acceleration curve
+        velocity.value += direction * DISTANCE * SPEED;
+        // We are adding more velocity this frame, so reset the drag timer
+        drag.elapsed_time = 0.0;
+    } else {
+        // We are not adding more velocity this frame, so increment the drag timer
+        drag.elapsed_time += time.delta_secs();
     }
 }
